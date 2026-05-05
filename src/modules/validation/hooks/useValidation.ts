61a82f9dcda7d128728f6b743923bb075569e2ac
Dido Kapanga <kapangadido@gmail.com>
2026-05-05 14:43:26 +0100

    Fix SMS send

--- src/modules/validation/hooks/useValidation.ts
+++ src/modules/validation/hooks/useValidation.ts
@@ -44,7 +44,7 @@ export const useValidateOperation = () => {
     {
       message: string;
       statut: string;
-      code_secret?: string | null;
+      // code_secret?: string | null;
     },
     Error,
     ValidateOperationDto
@@ -62,63 +62,3 @@ export const useValidateOperation = () => {
     },
   });
 };
-
-
-// // src/modules/validation/hooks/useValidation.ts
-
-// import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-// import {
-//   getTransfertsToValidate,
-//   validateOperation,
-// } from "../services/validation.service";
-// import type { TransfertClient } from "../services/validation.service";
-
-// // ✅ TYPE RESPONSE
-// type PaginatedResponse<T> = {
-//   data: T[];
-//   meta: {
-//     page: number;
-//     limit: number;
-//     total: number;
-//     totalPages: number;
-//   };
-// };
-
-// // ✅ OPTIONS TYPE SAFE
-// type QueryOptions = {
-//   enabled?: boolean;
-// };
-
-// // LIST
-// export const useValidationList = (
-//   page: number,
-//   limit: number,
-//   options?: QueryOptions
-// ) =>
-//   useQuery<PaginatedResponse<TransfertClient>>({
-//     queryKey: ["validation", page, limit],
-//     queryFn: () => getTransfertsToValidate(page, limit),
-//     ...options,
-//   });
-
-// // VALIDATE
-// export const useValidateOperation = () => {
-//   const qc = useQueryClient();
-
-//   return useMutation({
-//     mutationFn: validateOperation,
-//     onSuccess: () => {
-//       qc.invalidateQueries({
-//         queryKey: ["validation"],
-//       });
-
-//       qc.invalidateQueries({
-//         queryKey: ["transferts-caisse-process"],
-//       });
-
-//       qc.invalidateQueries({
-//         queryKey: ["transferts-caisse"],
-//       });
-//     },
-//   });
-// };
